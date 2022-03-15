cafa1e2cb70c33a38f4af062712c781a3a58d090
Manny <martinez.baker@yahoo.com>
2022-03-15 11:19:43 -0400

    made requested spaces in imports/exports

--- crypto-app/src/utils/utils.js
+++ crypto-app/src/utils/utils.js
@@ -11,11 +11,11 @@ export const formatTimePercent = (t) => {
 export const formatNum = (n) => {
   if (n < 1e3) return n;
   if (n === null) return <span>&infin;</span>;
-  if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
-  if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
-  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
-  if (n >= 1e12 && n < 1e15) return +(n / 1e12).toFixed(1) + "T";
-  if (n >= 1e15) return +(n / 1e15).toFixed(1) + "QD";
+  if (n >= 1e3 && n < 1e6) return (n / 1e3).toFixed(1) + "K";
+  if (n >= 1e6 && n < 1e9) return (n / 1e6).toFixed(1) + "M";
+  if (n >= 1e9 && n < 1e12) return (n / 1e9).toFixed(1) + "B";
+  if (n >= 1e12 && n < 1e15) return (n / 1e12).toFixed(1) + "T";
+  if (n >= 1e15) return (n / 1e15).toFixed(1) + "QD";
 };
 
 export function labelAlgo(arr, finalLength) {
