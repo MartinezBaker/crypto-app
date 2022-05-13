e3a7b8ea60378a6d48128fa6b94ca122ce5344d8
Manny <martinez.baker@yahoo.com>
2022-05-13 11:14:41 -0400

    added parse float to currency values

--- crypto-app/src/utils/functionUtils.js
+++ crypto-app/src/utils/functionUtils.js
@@ -35,11 +35,11 @@ export const formatPercent = (t) => {
 export const formatNum = (n) => {
   if (n < 1e3) return n.toFixed(0);
   if (n === null) return <span>&infin;</span>;
-  if (n >= 1e3 && n < 1e6) return (n / 1e3).toFixed(2) + "K";
-  if (n >= 1e6 && n < 1e9) return (n / 1e6).toFixed(2) + "M";
-  if (n >= 1e9 && n < 1e12) return (n / 1e9).toFixed(2) + "B";
-  if (n >= 1e12 && n < 1e15) return (n / 1e12).toFixed(2) + "T";
-  if (n >= 1e15) return (n / 1e15).toFixed(2) + "QD";
+  if (n >= 1e3 && n < 1e6) return parseFloat((n / 1e3).toFixed(2)) + "K";
+  if (n >= 1e6 && n < 1e9) return parseFloat((n / 1e6).toFixed(2)) + "M";
+  if (n >= 1e9 && n < 1e12) return parseFloat((n / 1e9).toFixed(2)) + "B";
+  if (n >= 1e12 && n < 1e15) return parseFloat((n / 1e12).toFixed(2)) + "T";
+  if (n >= 1e15) return parseFloat((n / 1e15).toFixed(2)) + "QD";
   if ("-") return; 
 };
 
